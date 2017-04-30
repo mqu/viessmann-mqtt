6368b20598087d89e3e5957a9423a448ad1a9ea3
Marc QUINTON <m.quinton@gmail.com>
2017-04-30 09:16:22 +0200

    raw commands works and some other changes

--- src/extra/viessmann-tcpclient-raw.rb
+++ src/extra/viessmann-tcpclient-raw.rb
@@ -153,3 +153,28 @@ exit 0
 
 #}
 
+# other addresses :
+# - https://www.bricozone.fr/t/interface-vitodens-200-avec-raspberry-pi.14671/page-9
+# - hollyday start-stop
+# 
+#     2309, 8 bytes, pour le départ vacances (YMDxhms)
+#     2311, 8 bytes, pour fin vacances (YMDxhms)
+# 
+# Fonctionne en lecture et écriture.
+# En lecture faire une conversion base 10 vers hexa
+# En écriture faire une conversion hexa vers base 10
+# 
+# Je ne pense pas que je câblerai la fonction sous Vitalk ou en PHP, car j'utilise de moins en moins l'application web.
+# 
+# Pour changer la date et l'heure sur la chaudière, on peut le faire assez facilement. Un simple script en python appelé par crontab permet de synchroniser l'heure, adresse 088E, 8 bytes (YMDxhms)
+# En lecture faire une conversion base 10 vers hexa
+# En écriture faire une conversion hexa vers base 10
+# 
+# x est le jour de la semaine 1 lundi et 7 dimanche.
+# 
+# Pour ceux qui paramètrent codage 1 ou 2 à partir de l'application, pour la puissance bruleur vous avez l'adresse mémoire 8832, sur 1 bytes (0 à 100).
+
+# plages d'adresses pour plages horaires :
+# 2000/8, 2230/8
+# php-script : http://easydomoticz.com/forum/viewtopic.php?f=17&t=1955&start=220
+# https://homematic-forum.de/forum/viewtopic.php?f=48&t=21640&start=30
